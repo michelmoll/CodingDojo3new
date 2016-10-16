6271982ff15f17add30528308c85d7690e87da5f
Michel Moll <michel.moll.1982@gmail.com>
2016-10-16 23:55:20 +0200

    Made GUI & Model Class

---
+++ CodingDojo3/Models/SoftwarePackages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo3.Models
+{
+    public class SoftwarePackages
+    {
+        
+
+        public String Name { get; set; }
+        public String Group { get; set; }
+        public float SalesPrice { get; set; }
+        public float PurchasePrice { get; set; }
+        public int OnStock { get; set; }
+
+        public SoftwarePackages(string name, string group, float salesPrice, float purchasePrice, int onStock)
+        {
+            Name = name;
+            Group = group;
+            SalesPrice = salesPrice;
+            PurchasePrice = purchasePrice;
+            OnStock = onStock;
+        }
+    }
+}
---
+++ CodingDojo3/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo3.ViewModels
+{
+    class MainViewModel
+    {
+        private string newName;
+
+        public string NewName
+        {
+            get { return newName; }
+            set { newName = value; }
+        }
+        private ObservableCollection<String> softwareList;
+
+        public ObservableCollection<String> SoftwareList
+        {
+            get { return softwareList; }
+            set { softwareList = value; }
+        }
+
+
+    }
+}
