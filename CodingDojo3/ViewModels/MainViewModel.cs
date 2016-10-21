eedc230bab5fb6e22a6f74bfd3c22277cf701f86
Michel Moll <michel.moll.1982@gmail.com>
2016-10-21 21:53:41 +0200

    Implementet missing part in the ViewModels, Builded converter for OnSock, mad the callcution for currency change.

--- CodingDojo3/ViewModels/MainViewModel.cs
+++ CodingDojo3/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CodingDojo4DataLib;
+using CodingDojo4DataLib.Converter;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,14 +10,40 @@ using System.Threading.Tasks;
 namespace CodingDojo3.ViewModels
 {
     class MainViewModel : BaseViewModel
-    {
+    {   
+        public Array Currencies
+        {
+            get { return Enum.GetValues(typeof(Currencies)); }
+        }
+
+        public Currencies SelectedCurrency
+        {
+            get { return selectedCurrency;}
+            set
+            {
+                selectedCurrency = value;
+                Onchange("SelectedCurrency");
+                StartConversion();
+            }
+        }
+
+        private void StartConversion()
+        {
+            foreach (var item in Items)
+            {
+                item.CalculateSalesPriceFromEuro(SelectedCurrency);
+                item.CalculatePurchasePriceFromEuro(SelectedCurrency);
+            }
+        }
+
         private List<StockEntry> stock;
 
         private ObservableCollection<StockEntryViewModel> items = new ObservableCollection<StockEntryViewModel>();
+        private Currencies selectedCurrency;
 
         public ObservableCollection<StockEntryViewModel> Items
         {
-            get {return Items;}
+            get {return items;}
             set
             {
                 items = value;
@@ -25,6 +52,7 @@ namespace CodingDojo3.ViewModels
         public MainViewModel()
         {
             SampleManager manager = new SampleManager();
+
             stock = manager.CurrentStock.OnStock;
             foreach (var item in manager.CurrentStock.OnStock)
             {
--- CodingDojo3/ViewModels/StockEntryViewModel.cs
+++ CodingDojo3/ViewModels/StockEntryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CodingDojo4DataLib;
+using CodingDojo4DataLib.Converter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,8 @@ namespace CodingDojo3.ViewModels
     class StockEntryViewModel :BaseViewModel
     {
         private StockEntry stockEntry;
-
         private double salespriceInEuro;
+        private double purchasepriceInEuro;
         public String Name
         {
             get { return stockEntry.SoftwarePackage.Name; }
@@ -19,9 +20,19 @@ namespace CodingDojo3.ViewModels
             {
                 stockEntry.SoftwarePackage.Name = value;
                 Onchange("Name");
-
             }
         }
+
+        public String Group
+        {
+            get { return stockEntry.SoftwarePackage.Category.Name; }
+            set
+            {
+                stockEntry.SoftwarePackage.Category.Name = value;
+                Onchange("Group");
+            }
+        }
+
 
         public double SalesPrice
         {
@@ -43,10 +54,41 @@ namespace CodingDojo3.ViewModels
             }
         }
 
+        public int OnStock
+        {
+            get { return stockEntry.Amount; }
+            set
+            {
+                stockEntry.Amount = value;
+                Onchange("OnStock");
+            }
+        }
+
+        public StockEntryViewModel()
+        {
+            this.stockEntry = new StockEntry();
+            this.stockEntry.SoftwarePackage = new Software("");
+            this.stockEntry.SoftwarePackage.Category = new Group();
+            this.stockEntry.SoftwarePackage.Category.Name = "New Category";
+        }
+
         public StockEntryViewModel(StockEntry entry)
         {
             stockEntry = entry;
+            salespriceInEuro = entry.SoftwarePackage.SalesPrice;
+            purchasepriceInEuro = entry.SoftwarePackage.PurchasePrice;
         }
+
+        public void CalculateSalesPriceFromEuro(Currencies currency)
+        {
+            this.SalesPrice = CurrencyConverter.ConvertFromEuroTo(currency, salespriceInEuro);
+        }
+
+        public void CalculatePurchasePriceFromEuro(Currencies currency)
+        {
+            this.PurchasePrice = CurrencyConverter.ConvertFromEuroTo(currency, purchasepriceInEuro);
+        }
+
 
     }
 }
